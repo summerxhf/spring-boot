67a42bf0bf7f3764879c9db5a15884af7b393960
XINGHAIFANG <xinghaifang@kingsoft.com>
2018-01-19 11:20:18 +0800

    yong laji

--- spring-boot-webinfo/src/main/java/com.hf.controller/SimpleController.java
+++ spring-boot-webinfo/src/main/java/com.hf.controller/SimpleController.java
@@ -3,18 +3,12 @@ package com.hf.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.core.io.Resource;
 import org.springframework.core.io.ResourceLoader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
-import sun.misc.IOUtils;
-
-import java.io.BufferedReader;
+import org.apache.commons.io.IOUtils;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
 
 @RestController
 @EnableAutoConfiguration
@@ -25,22 +19,8 @@ public class SimpleController {
     @RequestMapping("/WEB-INF-file")
     @ResponseBody
     public String testWebinfo() throws IOException{
-        Resource resource = resourceLoader.getResource("/WEB-INF/target_file.txt");
-        String content="";
-        try{
-            InputStream is = resource.getInputStream();
-            BufferedReader br = new BufferedReader(new InputStreamReader(is));
-            String line;
-            while ((line = br.readLine()) != null) {
-                System.out.println(line);
-                content = content + line;
-            }
-            br.close();
-        }catch(IOException e){
-            e.printStackTrace();
-        }
-
-        return "the webinfo directory of file : " + content;
+        String content = IOUtils.toString(resourceLoader.getResource("classpath:WEB-INF/target_file.txt").getInputStream());
+        return "the content of resources:" + content;
     }
 
     public static void main(String[] args) {
